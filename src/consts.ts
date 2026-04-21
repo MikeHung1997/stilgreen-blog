a0d19dd668fd0e53f6f05ad0cb78fef433a6f30c
MikeHung1997 <mikehung425@gmail.com>
2026-04-21 17:05:25 +0800

    change title

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Still Green';
+export const SITE_DESCRIPTION = 'Green energy knowledge, eco-friendly product reviews and sustainable living tips. Find the best solar, home energy and eco products.';
