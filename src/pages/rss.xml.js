8bf74ff7014e850aa54b9b798f45b1f878d3f7c3
MikeHung1997 <91788026+MikeHung1997@users.noreply.github.com>
2026-04-30 00:09:31 +0800

    add rss image

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -5,12 +5,28 @@ import { SITE_DESCRIPTION, SITE_TITLE } from '../consts';
 export async function GET(context) {
 	const posts = await getCollection('blog');
 	return rss({
-		title: SITE_TITLE,
-		description: SITE_DESCRIPTION,
-		site: context.site,
-		items: posts.map((post) => ({
-			...post.data,
-			link: `/blog/${post.id}/`,
-		})),
-	});
+    title: SITE_TITLE,
+    description: SITE_DESCRIPTION,
+    site: context.site,
+    xmlns: { 
+      media: 'http://search.yahoo.com/mrss/',
+    },
+    items: posts.map((post) => {
+		
+      const item = {
+        title: post.data.title,
+        description: post.data.description,
+        pubDate: post.data.pubDate,
+        link: `/blog/${post.id}/`,
+      };
+      
+      if (post.data.heroImage) {
+        const imageUrl = new URL(post.data.heroImage.src, context.site).href;
+        item.customData = `<media:content url="${imageUrl}" medium="image" />`;
+      }
+      
+      return item;
+    }),
+    customData: `<language>zh-TW</language>`,
+  });
 }
